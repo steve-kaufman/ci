2abb7b139ec88e088c9414331588305bed8709be
Steven <steveplaysguiola@gmail.com>
2021-03-30 17:27:18 -0500

    updated main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
 )
@@ -11,6 +12,8 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received a request to deploy '%s'", r.URL.Path)
+
 	switch r.URL.Path {
 	case "/eshop-frontend":
 		deployEshopFrontend(w)
@@ -18,20 +21,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func deployEshopFrontend(w http.ResponseWriter) {
+	fmt.Println("Deploying Eshop Frontend")
+
+	fmt.Println("Pulling new image")
 	err := exec.Command("docker", "pull", "stevekaufman/eshop-frontend").Run()
 	if err != nil {
 		sendError(w, "Failed to deploy eshop frontend")
 	}
+	fmt.Println("Pulled new image")
 
+	fmt.Println("Stopping old container")
 	err = exec.Command("docker", "stop", "eshop-frontend").Run()
 	if err != nil {
 		return
 	}
+	fmt.Println("Stopped old container")
 
+	fmt.Println("Starting new container")
 	err = exec.Command("docker", "run", "-d", "--rm", "--name", "eshop-frontend", "-p", "8085:8080", "stevekaufman/eshop-frontend").Run()
 	if err != nil {
 		sendError(w, err.Error())
 	}
+	fmt.Println("Started new container")
 }
 
 func sendError(w http.ResponseWriter, msg string) {
