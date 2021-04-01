3f701c3165e2426100b9256d7f5a01fc477f9912
Steven <steveplaysguiola@gmail.com>
2021-04-01 13:48:16 -0500

    refactored for any docker image that exposes port 8080

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-  log.Fatal(http.ListenAndServe(":5500", http.HandlerFunc(handler)))
+	log.Fatal(http.ListenAndServe(":5500", http.HandlerFunc(handler)))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,36 +16,57 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.URL.Path {
 	case "/eshop-frontend":
-		deployEshopFrontend(w)
+		deployDockerImage(w, "stevekaufman/eshop-frontend", 8085)
+	case "/website":
+		deployDockerImage(w, "stevekaufman/website", 5000)
+	default:
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Nothing to deploy at '%s'", r.URL.Path)
 	}
 }
 
-func deployEshopFrontend(w http.ResponseWriter) {
-	fmt.Println("Deploying Eshop Frontend")
+func deployDockerImage(w http.ResponseWriter, imageName string, port int) {
+	fmt.Printf("Deploying '%s'", imageName)
 
 	fmt.Println("Pulling new image")
-	err := exec.Command("docker", "pull", "stevekaufman/eshop-frontend").Run()
+	err := exec.Command("docker", "pull", imageName).Run()
 	if err != nil {
-		sendError(w, "Failed to deploy eshop frontend")
+		sendError(w, fmt.Sprintf("Failed to deploy '%s'", imageName))
 	}
 	fmt.Println("Pulled new image")
 
 	fmt.Println("Stopping old container")
-	err = exec.Command("docker", "stop", "eshop-frontend").Run()
+	err = exec.Command("docker", "stop", imageName).Run()
 	if err != nil {
 		fmt.Println("No container already running")
 	}
 	fmt.Println("Stopped old container")
 
+	fmt.Println("Removing old container just in case")
+	err = exec.Command("docker", "rm", imageName).Run()
+	if err != nil {
+		fmt.Println("Container did not need removing")
+	} else {
+		fmt.Println("Container removed")
+	}
+
+	portMap := fmt.Sprintf("%d:8080", port)
+
 	fmt.Println("Starting new container")
-	err = exec.Command("docker", "run", "-d", "--rm", "--name", "eshop-frontend", "-p", "8085:8080", "stevekaufman/eshop-frontend").Run()
+	err = exec.Command("docker", "run", "-d", "--rm", "--name", imageName, "-p", portMap, imageName).Run()
 	if err != nil {
 		sendError(w, err.Error())
 	}
 	fmt.Println("Started new container")
+
+	sendSuccess(w, fmt.Sprintf("Deployed '%s'", imageName))
 }
 
 func sendError(w http.ResponseWriter, msg string) {
 	w.WriteHeader(400)
+	fmt.Fprint(w, msg)
+}
+
+func sendSuccess(w http.ResponseWriter, msg string) {
 	fmt.Fprint(w, msg)
 }
